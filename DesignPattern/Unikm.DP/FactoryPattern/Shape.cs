bae8a46c6b063423ed83160a72d9cef5436b4af4
yuzhoukamen <595667627@qq.com>
2019-02-25 17:35:31 +0800

    工厂模式创建

---
+++ DesignPattern/Unikm.DP/FactoryPattern/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unikm.DP.FactoryPattern
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Shape
+    {
+    }
+}
