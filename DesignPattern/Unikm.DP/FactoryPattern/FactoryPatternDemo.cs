8ca1b3bc44d18c373736cd474ff3d0a66d1bffff
yuzhoukamen <595667627@qq.com>
2019-02-25 18:02:27 +0800

    FactoryPatternDemo update

--- DesignPattern/Unikm.DP/FactoryPattern/FactoryPatternDemo.cs
+++ DesignPattern/Unikm.DP/FactoryPattern/FactoryPatternDemo.cs
@@ -11,27 +11,25 @@ namespace Unikm.DP.FactoryPattern
     /// </summary>
     public class FactoryPatternDemo
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(String[] args)
         {
-            ShapeFactory shapeFactory = new ShapeFactory();
+            ShapeFactory shapeFactory = new ShapeFactory();//使用该工厂，通过传递类型信息来获取实体类的对象。
 
-            //获取 Circle 的对象，并调用它的 draw 方法
-            IShape shape1 = shapeFactory.GetShape("CIRCLE");
+            IShape shape1 = shapeFactory.GetShape("CIRCLE");//获取 Circle 的对象，并调用它的 draw 方法
 
-            //调用 Circle 的 draw 方法
-            shape1.Draw();
+            shape1.Draw();//调用 Circle 的 draw 方法
+            
+            IShape shape2 = shapeFactory.GetShape("RECTANGLE");//获取 Rectangle 的对象，并调用它的 draw 方法
+            
+            shape2.Draw();//调用 Rectangle 的 draw 方法
+            
+            IShape shape3 = shapeFactory.GetShape("SQUARE");//获取 Square 的对象，并调用它的 draw 方法
 
-            //获取 Rectangle 的对象，并调用它的 draw 方法
-            IShape shape2 = shapeFactory.GetShape("RECTANGLE");
-
-            //调用 Rectangle 的 draw 方法
-            shape2.Draw();
-
-            //获取 Square 的对象，并调用它的 draw 方法
-            IShape shape3 = shapeFactory.GetShape("SQUARE");
-
-            //调用 Square 的 draw 方法
-            shape3.Draw();
+            shape3.Draw();//调用 Square 的 draw 方法
 
             Console.ReadLine();
         }
