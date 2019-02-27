f5589335fe53a805dffd7ce5a7778874a5fdeb0f
yuzhoukamen <595667627@qq.com>
2019-02-27 15:37:37 +0800

    创建一个工厂创造器/生成器类，通过传递形状或颜色信息来获取工厂。

---
+++ DesignPattern/Unikm.DP/AbstractFactoryPattern/ColorFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unikm.DP.AbstractFactoryPattern
+{
+    public class ColorFactory : AbstractFactory
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public override IColor GetColor(string color)
+        {
+            if (color == null)
+            {
+                return null;
+            }
+            if (color.ToUpper().Equals("RED"))
+            {
+                return new Red();
+            }
+            else if (color.ToUpper().Equals("GREEN"))
+            {
+                return new Green();
+            }
+            else if (color.ToUpper().Equals("BLUE"))
+            {
+                return new Blue();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        public override IShape GetShape(string shape)
+        {
+            return null;
+        }
+    }
+}
---
+++ DesignPattern/Unikm.DP/AbstractFactoryPattern/FactoryProducer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unikm.DP.AbstractFactoryPattern
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FactoryProducer
+    {
+        /// <summary>
+        /// 创建一个工厂创造器/生成器类，通过传递形状或颜色信息来获取工厂。
+        /// </summary>
+        /// <param name="choice"></param>
+        /// <returns></returns>
+        public static AbstractFactory GetFactory(String choice)
+        {
+            if (choice.ToUpper().Equals("SHAPE"))
+            {
+                return new ShapeFactory();
+            }
+            else if (choice.ToUpper().Equals("COLOR"))
+            {
+                return new ColorFactory();
+            }
+            return null;
+        }
+    }
+}
