9fb676af193a16c0409b98b6f031fe0dce70a34b
yuzhoukamen <595667627@qq.com>
2019-03-02 10:40:43 +0800

    增加demo

---
+++ BuilderPattern/BuilderPatternDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BuilderPatternDemo
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(String[] args)
+        {
+            MealBuilder mealBuilder = new MealBuilder();
+
+            Meal vegMeal = mealBuilder.prepareVegMeal();
+
+            Console.WriteLine("Veg Meal");
+
+            vegMeal.showItems();
+
+            Console.WriteLine("Total Cost: " + vegMeal.getCost());
+
+            Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+
+            Console.WriteLine("\n\nNon-Veg Meal");
+
+            nonVegMeal.showItems();
+
+            Console.WriteLine("Total Cost: " + nonVegMeal.getCost());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BuilderPattern/MealBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MealBuilder
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Meal prepareVegMeal()
+        {
+            Meal meal = new Meal();
+
+            meal.addItem(new VegBurger());
+            meal.addItem(new Coke());
+
+            return meal;
+        }
+
+        public Meal prepareNonVegMeal()
+        {
+            Meal meal = new Meal();
+
+            meal.addItem(new ChickenBurger());
+            meal.addItem(new Pepsi());
+
+            return meal;
+        }
+    }
+}
