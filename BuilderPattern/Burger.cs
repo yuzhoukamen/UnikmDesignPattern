3ee328d0a1cf8451a1ddc907c3dfb0bc3529f793
yuzhoukamen <595667627@qq.com>
2019-03-02 10:23:54 +0800

    add Burger and ColdDrink

---
+++ BuilderPattern/Burger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    /// <summary>
+    /// 创建实现 Item 接口的抽象类，该类提供了默认的功能。
+    /// </summary>
+    public abstract class Burger : IItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string Name()
+        {
+            return "Burger";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IPacking Packing()
+        {
+            return new Wrapper();
+        }
+
+        public abstract float Price();
+    }
+}
---
+++ BuilderPattern/ColdDrink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    public abstract class ColdDrink : IItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string Name()
+        {
+            return "ColdDrink";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IPacking Packing()
+        {
+            return new Bottle();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public abstract float Price();
+    }
+}
